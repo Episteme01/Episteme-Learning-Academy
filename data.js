3aee6da5592baf4258553a9d1e843d0ea42f6f5b
Episteme01 <74595999+Episteme01@users.noreply.github.com>
2026-02-10 18:47:22 +0100

    Add files via upload

---
+++ data.js
@@ -0,0 +1,13 @@
+const students = [
+  {
+    id: "ELA001",
+    name: "Akinyemi Maisarah",
+    class: "Pre-School",
+    attendance: "90%",
+    assessments: {
+      Literacy: 85,
+      Numeracy: 80
+    },
+    remarks: "Great improvement and active participation."
+  }
+];
